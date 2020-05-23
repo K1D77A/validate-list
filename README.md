8a72c1ae443bc314dac7d22b273560910f9f55c2
K1D77A <k1llfrenzy@Hotmail.co.uk>
2020-05-23 03:01:42 +0100

    added documentation.

--- README.md
+++ README.md
@@ -1,7 +1,46 @@
 # validate-list
-### _Your Name <your.name@example.com>_
 
-This is a project to do ... something.
+This is an attempt at using templates to validate the contents of a list. 
+The idea came because when you parse json from an untrusted source you don't know for certain what 
+has been sent, so the idea is that once parsed you can use a template to check if the parsed data 
+(now a list) conforms to what you want. There is only one function and that is 
+``validate-list-p`` Which takes in two arguments, the list you wish to validate and a template.
+Here is the docstring: 
+``
+"takes in a list that you want to validate, and a template, the template is a list of lists,
+each list contains keywords and their values (a list of the keywords is in *valid-syms*). Each list
+within the template represents 1 element in the template and is a 'description' of its contents. 
+For example given the template '((:equal \"key\") (:type string :maxlen 40)) this could be used
+to validate the list '(\"key\" \"abcdeegadfgfsdf\") because as the template says, the first item in 
+list is \"key\" and the second according to the template should be of type 'string and no longer
+than 40 characters long, which it is not, so this is valid and will return t, if a list fails when
+checked against the template then this func returns nil. another example list and template can be 
+found in *test-list2* and *test-template2* respectively"
+``
+
+These are two example lists and their templates: 
+```lisp
+(defparameter *test-list1* '("key" "abcdeegadfgfsdf"))
+(defparameter *test-template1* '((:equal "key") (:type string :maxlen 40)))
+
+(defparameter *test-list2*  '("year" 2020 ("country" "USA")))
+(defparameter *test-template2* '((:equal "year")(:type integer :between (2100 1900))
+                                 ((:or ("cookie" "country"))(:type string :maxlen 50))))
+```
+Here are the other keys and what they do (each key takes exactly 1 argument):
+
+* :equal - checks if list entry is equalp to arg.
+* :type - checks if list entry is of type arg.
+* :between - check if list entry is between the numbers in the list arg ie between '(3 5)
+* :minlen - checks if list entry is at least length arg.
+* :maxlen - checks if list entry between length 0 and arg. 
+* :less-than - checks if list entry is a number and is lower than arg.
+* :greater-than - checks if list entry is a number and is greater than arg.
+* :or - checks if list entry is equalp to any items within the list arg ie '("hello" "help" "house")
+* :satisfies - checks if the function in arg returns t when called with the list entry
+
+
+
 
 ## License
 
--- src/validate-list.lisp
+++ src/validate-list.lisp
@@ -10,13 +10,16 @@
 ;;;;a maximum of length 40 well this library attempts to allow you to construct templates
 ;;;;of what you would expect, for example parsed that json would look something like
 ;;;;("key" "abcdeegadfgfsdf") now a template for this would look something like
-;;;;'(("key") (:type string :maxlen 40)) the length indicates two arguments
+;;;;'((:equal "key") (:type string :maxlen 40)) the length indicates two arguments
 ;;;;another example could ("year" 2020 ("country" "USA")) where a valid template would look like
-;;;;'(("year")(:type integer :between (2100 1900))
-;;;;  (("country")(:type string :maxlen 50))
+;;;;'((:equal "year")(:type integer :between (2100 1900))
+;;;;  ((:equal "country")(:type string :maxlen 50))
 
 ;;;;proposed symbols
-;;;; :equal :type :between :minlen :maxlen :less-than :greater-than :or :satisfies
+(defparameter *valid-syms* '(:equal :type :between :minlen
+                             :maxlen :less-than :greater-than
+                             :or :satisfies))
+
 (defparameter *test-list1* '("key" "abcdeegadfgfsdf"))
 (defparameter *test-template1* '((:equal "key") (:type string :maxlen 40)))
 
@@ -40,7 +43,6 @@
 ;;   (lisp-unit:assert-false (handle-minlen "" 3))
 ;;   (lisp-unit:assert-error 'simple-error (handle-minlen 1 1))
 ;;   (lisp-unit:assert-error 'simple-error (handle-minlen "1" -1)))
-
 
 (defun handle-maxlen (entry maxlen)
   (check-type maxlen integer)
@@ -105,31 +107,36 @@
     (:otherwise    (error "invalid keyword"))));;change to a condition
 
 (defun process-template-entry (template-entry entry)
-  ;;(:equal "key")
-  ;;or  (:type string :maxlen 40)
-  ;;and the entry "key" or a string as examples
   (map-plist (lambda (sym arg)
                (call-right-fun-from-sym sym arg entry))
              template-entry))
 
+(defun validate-list-p (list template)
+  "takes in a list that you want to validate, and a template, the template is a list of lists,
+each list contains keywords and their values (a list of the keywords is in *valid-syms*). Each list
+within the template represents 1 element in the template and is a 'description' of its contents. 
+For example given the template '((:equal \"key\") (:type string :maxlen 40)) this could be used
+to validate the list '(\"key\" \"abcdeegadfgfsdf\") because as the template says, the first item in 
+list is \"key\" and the second according to the template should be of type 'string and no longer
+than 40 characters long, which it is not, so this is valid and will return t, if a list fails when
+checked against the template then this func returns nil. another example list and template can be 
+found in *test-list2* and *test-template2* respectively"
+  (check-type list list)
+  (check-type template list)
+  (if (/= (length list)(length template))
+      nil
+      (labels ((rec (list template acc)
+                 ;; (format t "~&entry: ~A~%"list)
+                 ;; (format t "~&template ~A~%" template)
+                 (cond ((or (null list)
+                            (null template))
+                        nil)
+                       ((listp (first template))
+                        (append (rec (first list) (first template) acc)
+                                (rec (rest list) (rest template) acc)))
+                       (t (process-template-entry template list)))))
+        (not (some #'null (rec list template '()))))))
 
-(defun validate-list (list template)
-  (when (/= (length list)(length template))
-    (error "list and template are not the same length"));;replace with a condition or maybe just a nil
-  (labels ((rec (list template acc)
-             (format t "~&entry: ~A~%"list)
-             (format t "~&template ~A~%" template)
-             (cond ((or (null list)
-                        (null template))
-                    nil)
-                   ((listp (first template))
-                    (append (rec (first list) (first template) acc)
-                            (rec (rest list) (rest template) acc)))
-                   (t (process-template-entry template list)))))
-    (not (some #'null (rec list template '())))))
-  ;; (mapcar (lambda (template-entry list-entry)
-  ;;           (process-template-entry template-entry list-entry))
-  ;;         template list))
 
 
 
